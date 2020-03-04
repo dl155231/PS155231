310be29607f9572f9b8eec9f00ac314309ac912c
dl155231 <61503932+dl155231@users.noreply.github.com>
2020-03-04 15:19:16 +0100

    Create 1_4_9.c

---
+++ 04_03_19/1_4_9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int n,m;
+	scanf("%i %i", &n, &m);
+	int pom;
+	while (m != 0) {
+		pom = m;
+		m = n % m;
+		n = pom;
+	}
+	printf("%i", n);
+}
