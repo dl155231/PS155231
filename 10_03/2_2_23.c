dc94e7033637959adef436598bf328518f55a3fa
dl155231 <61503932+dl155231@users.noreply.github.com>
2020-03-10 18:18:30 +0100

    Update 2_2_23.c

--- 10_03/2_2_23.c
+++ 10_03/2_2_23.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int fibonacci(int n) {
 	if (n <= 1) {
 		return 1;
 	}
-	else
-		return fibonacci(n - 1) + fibonacci(n - 2);;
+	else return fibonacci(n - 1) + fibonacci(n - 2);;
 }
 int main()
 {
 	int n = 0;
 	scanf("%i", &n);
-	printf("%i",fibonacci(n));
+	printf("%i", fibonacci(n));
 	return 0;
 }
