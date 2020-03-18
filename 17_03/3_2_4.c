4610680bc0fc9c0153cd7cabf4ab76ed762ce5aa
dl155231 <61503932+dl155231@users.noreply.github.com>
2020-03-18 17:25:16 +0100

    dodanie drugiego wywolania

--- 17_03/3_2_4.c
+++ 17_03/3_2_4.c
@@ -11,7 +11,10 @@ void zamiana(int *x, int *y) {
 }
 int main() {
 	int a = 2, b = 1;
+	int x = 10, y = 20;
 	zamiana(&a, &b);
-	printf("%i %i", a, b);
+	printf("%i %i\n", a, b);
+	zamiana(&x, &y);
+	printf("%i %i\n", x, y);
 	return 0;
 }
