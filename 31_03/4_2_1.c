bfb735ba87e3dde7f3750c02540c0f74491245c6
dl155231 <61503932+dl155231@users.noreply.github.com>
2020-05-02 11:51:42 +0200

    Update 4_2_1.c

--- 31_03/4_2_1.c
+++ 31_03/4_2_1.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int foo1(int n, int *tab){
+    for(int i=0; i<n; i++)
+        tab[i]=0;
+    return tab;
+}
+
+int foo2(int n, int *tab){
+    for(int i=0; i<n; i++)
+        tab[i]=i;
+    return tab;
+}
+
+int foo3(int n, int *tab){
+    for(int i=0; i<n; i++)
+        tab[i]*=2;
+    return tab;
+}
+
+int foo4(int n, int *tab){
+    for(int i=0; i<n; i++)
+        if(tab[i]<0) tab[i]*=-1;
+    return tab;
+}
+
+void wypisz(int n, int tab[]){
+    for(int i=0; i<n; i++)
+        printf("%i \t", tab[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    scanf("%i", &n);
+    int tab[n];
+    wypisz(n, foo1(n, &tab));
+    wypisz(n, foo2(n, &tab));
+    wypisz(n, foo3(n, &tab));
+    wypisz(n, foo4(n, &tab));
+    return 0;
+}
